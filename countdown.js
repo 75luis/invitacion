8f2d87865a894b10ea0936b56c306b5b26def104
LuisChaparin <luis.chaparin@gmail.com>
2024-12-24 21:57:51 -0500

    Fecha

--- countdown.js
+++ countdown.js
@@ -1,9 +1,9 @@
 //window.onload
 simplyCountdown('#cuenta', {
     year: 2025, // required
-    month: 12, // required
-    day: 1, // required
-    hours: 21, // Default is 0 [0-23] integer
+    month: 1, // required
+    day: 11, // required
+    hours: 19, // Default is 0 [0-23] integer
     minutes: 0, // Default is 0 [0-59] integer
     seconds: 0, // Default is 0 [0-59] integer
     words: { //words displayed into the countdown
